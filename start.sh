75787aa7820702d7fb6a28c665230af9b1082793
ertyrdd <136952785+ertyrdd@users.noreply.github.com>
2023-06-18 08:13:55 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
-
+export UUID=dbb44ec9-7184-41a0-8b77-8110fe5a6e06
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=arsenalfclatestnews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
